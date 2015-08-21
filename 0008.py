30ef3e070e7b01f427c74940fcbc51cab4609ff2
whix <mr.gblues@gmail.com>
2015-08-21 18:28:03 +0800

    add 0008.py

--- 0008.py
+++ 0008.py
@@ -1,10 +1,17 @@
-__author__ = 'Administrator'
-import re
-import requests
 from goose import Goose
+from goose.text import StopWordsChinese
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
-html = requests.get('http://www.xuebuyuan.com/2127057.html').content.decode('utf-8')
-print html
-lst = re.findall(r'', html)
+# html = requests.get('http://www.xuebuyuan.com/2127057.html').content.decode('utf-8')
+# print html
+# lst = re.findall(r'', html)
+url = 'http://www.xuebuyuan.com/2127057.html'
+def extract(url):
+    g = Goose({'stopwords_class': StopWordsChinese})
+    article = g.extract(url=url)
+    return article.cleaned_text
 
-print lst
+if __name__ == '__main__':
+    print extract(url)
